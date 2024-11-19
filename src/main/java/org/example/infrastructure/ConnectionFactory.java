70e3e4f3122eb595bf6d420b09c4350e9e536d76
RafaellSouzaPinto <rafaelsouza.rs14@gmail.com>
2024-11-19 12:57:12 -0300

    Alterando pra raily

--- src/main/java/org/example/infrastructure/ConnectionFactory.java
+++ src/main/java/org/example/infrastructure/ConnectionFactory.java
@@ -6,8 +6,15 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM555130","040506");
-    }
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
+    public static Connection getConnection() throws SQLException {
+        if (DB_URL == null || DB_USER == null || DB_PASSWORD == null) {
+            throw new SQLException("As variáveis de ambiente DB_URL, DB_USER e DB_PASSWORD não estão configuradas.");
+        }
+
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
 }
