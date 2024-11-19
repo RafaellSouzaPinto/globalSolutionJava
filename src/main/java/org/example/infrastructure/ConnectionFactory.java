7799b6c69a362d51e25cad476321ccca2fb00e15
RafaellSouzaPinto <rafaelsouza.rs14@gmail.com>
2024-11-19 13:08:11 -0300

    Voltando

--- src/main/java/org/example/infrastructure/ConnectionFactory.java
+++ src/main/java/org/example/infrastructure/ConnectionFactory.java
@@ -6,15 +6,8 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String DB_URL = System.getenv("DB_URL");
-    private static final String DB_USER = System.getenv("DB_USER");
-    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
-
     public static Connection getConnection() throws SQLException {
-        if (DB_URL == null || DB_USER == null || DB_PASSWORD == null) {
-            throw new SQLException("As variáveis de ambiente DB_URL, DB_USER e DB_PASSWORD não estão configuradas.");
-        }
-
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM555130","040506");
     }
+
 }
